1e23fc8ac00fe8bd53e1fc265777e0a4c65ab1af
Mohammad Jabir <77463773+Lock0NStrat0s@users.noreply.github.com>
2023-10-01 09:32:43 -0600

    homework required user to put first and last name in two different textboxes and then print in unordered list below.
    homework complete.

---
+++ BlazorServerHomework/Models/PersonModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorServerHomework.Models
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
--- BlazorServerHomework/Program.cs
+++ BlazorServerHomework/Program.cs
@@ -1,4 +1,3 @@
-using BlazorServerHomework.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
@@ -13,7 +12,6 @@ namespace BlazorServerHomework
             // Add services to the container.
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
-            builder.Services.AddSingleton<WeatherForecastService>();
 
             var app = builder.Build();
 
